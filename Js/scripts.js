d1471a50aee708bd514e91d33387d28b359a8da8
julieprograms <junge.julie@web.de>
2021-11-24 02:19:46 +0100

    reupload and fixing comments

--- Js/scripts.js
+++ Js/scripts.js
@@ -11,28 +11,28 @@ if(inputValue === ''){
   alert("You must write something!");
 } else {
   $('#list').append(li);
-  $('#input').val('');
+  $('#input').val('')
 }
 
 
 //crossing out items from list of items:
 function crossOut() {
-  li.toggleClass("strike");
+  li.toggleClass('strike');
 }
-//double function makes code longer here?can I drop first one??
+
 li.on('dblclick', crossOut);
 
 
 
 // adding a delete button 'x'
-let crossOutButton = $('<crossOutButton></crossOutButton>')
+let crossOutButton = $('<crossOutButton></crossOutButton>');
 crossOutButton.append(document.createTextNode('x'));
 li.append(crossOutButton);
 
 
 //adding CLASS DELETE (DISPLAY: NONE) from the css:
 function deleteListItem(){
-     li.addClass("delete");
+     li.addClass('delete');
    }
 crossOutButton.on('click', deleteListItem);
 
